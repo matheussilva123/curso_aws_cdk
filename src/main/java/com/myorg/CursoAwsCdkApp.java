3785d0a18d07a8e5d3953e44d8a49d53c995dd6e
matheus.dias <matheus.dias@ifood.com.br>
2022-01-07 19:57:41 -0300

    add new queue at Service02Stack

--- src/main/java/com/myorg/CursoAwsCdkApp.java
+++ src/main/java/com/myorg/CursoAwsCdkApp.java
@@ -23,7 +23,8 @@ public class CursoAwsCdkApp {
         service01Stack.addDependency(clusterStack);
         service01Stack.addDependency(rdsStack);
         service01Stack.addDependency(snsStack);
-        Service02Stack service02Stack = new Service02Stack(app, "Service02", clusterStack.getCluster());
+        Service02Stack service02Stack = new Service02Stack(app, "Service02", clusterStack.getCluster(), snsStack.getProductEventsTopic());
+        service02Stack.addDependency(snsStack);
 
 
         app.synth();
